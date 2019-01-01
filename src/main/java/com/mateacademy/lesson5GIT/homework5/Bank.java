8bb59935fc2feeca0c93a84c599f7b9ea679b9c3
Sergey Klunniy <s.klunniy@gmail.com>
2019-01-02 00:40:35 +0200

    add Adress Bank

--- src/main/java/com/mateacademy/lesson5GIT/homework5/Bank.java
+++ src/main/java/com/mateacademy/lesson5GIT/homework5/Bank.java
@@ -1,7 +1,7 @@
 package com.mateacademy.lesson5GIT.homework5;
 
 public class Bank<T> implements GenericInterface<T> {
-    
+    String adress = "Mate academy, Tarasivska St, 16, office 28, Kyiv, Ukraine, 01033";
     T t;
 
     @Override
