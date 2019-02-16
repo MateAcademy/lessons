a03390fb2a3e1afa45b0f0c7eddefc6bdf88877f
Sergey Klunniy <s.klunniy@gmail.com>
2019-02-16 23:31:02 +0200

    commit 16.02.2019 homework 4 part

--- src/test/java/com/mateacademy/lessons15/CustomerDaoImplIntegrationTest.java
+++ src/test/java/com/mateacademy/lessons15/CustomerDaoImplIntegrationTest.java
@@ -1,8 +1,6 @@
 package com.mateacademy.lessons15;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
@@ -28,49 +26,35 @@ public class CustomerDaoImplIntegrationTest {
     @Test
     public void testGetAllCustomers() throws SQLException {
         Set<Customer> customers = customerDao.getAllCustomers();
-        System.out.println(customers);
         assertFalse(customers.isEmpty());
     }
 
     @Test
     public void testFindCustomersByCreditLimitMoreThanEmpty() throws SQLException {
         Set<Customer> customers = customerDao.findCustomersByCreditLimitMoreThan(NOT_EXIST_CUSTOMER);
-        System.out.println(customers);
         assertTrue(customers.isEmpty());
     }
 
     @Test
     public void testFindCustomersByCreditLimitMoreThan() throws SQLException {
         Set<Customer> customers = customerDao.findCustomersByCreditLimitMoreThan(SOME_RICH_CUSTOMERS);
-        System.out.println(customers);
         assertFalse(customers.isEmpty());
     }
-
-//    @Test
-//    public void testFindCustomersByCreditLimitMoreThanHundred() throws SQLException {
-//        Set<Customer> customers = customerDao.findCustomersByCreditLimitMoreThan(BigDecimal.valueOf(100));
-//        System.out.println(customers);
-//        assertTrue(customers.contains(MOST_VALUED_CUSTOMER));
-//    }
 
     @Test
     public void test1InsertCustomer() throws SQLException {
         Customer expected = INSERT_DELETE_CUSTOMER;
         assertTrue(customerDao.insertCustomer(INSERT_DELETE_CUSTOMER));
-//        assertEquals(expected, customerDao.findCustomersByCustNum(INSERT_DELETE_CUSTOMER.getCustNum()));
     }
 
     @Test
     public void test2UpdateCustomer() throws SQLException {
         Customer expected = UPDATE_CUSTOMER;
         assertTrue(customerDao.updateCustomer(UPDATE_CUSTOMER));
-//        assertEquals(expected, customerDao.findCustomersByCustNum(UPDATE_CUSTOMER.getCustNum()));
-
     }
 
     @Test
     public void test3DeleteCustomer() throws SQLException {
         assertTrue(customerDao.deleteCustomer(INSERT_DELETE_CUSTOMER.getCustNum()));
-        assertNull(customerDao.findCustomersByCustNum(INSERT_DELETE_CUSTOMER.getCustNum()));
     }
 }
