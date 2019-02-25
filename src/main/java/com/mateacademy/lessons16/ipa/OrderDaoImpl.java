24fa6c99c238b7375e6e3d437ad2b70f79dad821
Sergey Klunniy <s.klunniy@gmail.com>
2019-02-25 15:58:26 +0200

    commit 25.02.2019 homework

--- src/main/java/com/mateacademy/lessons16/ipa/OrderDaoImpl.java
+++ src/main/java/com/mateacademy/lessons16/ipa/OrderDaoImpl.java
@@ -16,11 +16,82 @@ import java.util.Locale;
 import java.util.Set;
 
 public class OrderDaoImpl implements OrderDao {
+    private static EntityManagerFactory factory;
 
+    static {
+        Locale.setDefault(Locale.ENGLISH);
+        factory = Persistence.createEntityManagerFactory("PERSISTENCE");
+    }
 
+    private static final Logger LOG = LogManager.getLogger(com.mateacademy.lessons16.ipa.OrderDaoImpl.class);
 
+    private EntityManager entityManager = factory.createEntityManager();
 
+    @Override
+    public boolean insertOrder(Orders transientInstance) {
+        LOG.debug("persisting Orders instance");
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(transientInstance);
+            entityManager.getTransaction().commit();
+            LOG.debug("persist successful");
+            return true;
+        } catch (RuntimeException re) {
+            if (entityManager != null) {
+                System.out.println("Transaction is being rolled back.");
+                entityManager.getTransaction().rollback();
+            }
+            LOG.error("persist failed", re);
+            throw re;
+        }
+    }
 
+    @Override
+    public boolean deleteOrder(BigDecimal id) {
+        LOG.debug("removing Orders instance");
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.remove(entityManager.find(Orders.class, id));
+            entityManager.getTransaction().commit();
+            LOG.debug("remove successful");
+            return true;
+        } catch (RuntimeException re) {
+            if (entityManager != null) {
+                System.out.println("Transaction is being rolled back.");
+                entityManager.getTransaction().rollback();
+            }
+            LOG.error("remove failed", re);
+            throw re;
+        }
+    }
 
+    @Override
+    public boolean updateOrder(Orders detachedInstance) {
+        LOG.debug("merging Orders instance");
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(detachedInstance);
+            entityManager.getTransaction().commit();
+            LOG.debug("merge successful");
+            return true;
+        } catch (RuntimeException re) {
+            if (entityManager != null) {
+                System.out.println("Transaction is being rolled back.");
+                entityManager.getTransaction().rollback();
+            }
+            LOG.error("merge failed", re);
+            throw re;
+        }
+    }
+
+    @Override
+    public Set<Orders> getAllOrders() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Orders findOrderById(BigDecimal id) throws SQLException {
+        return null;
+    }
 
 }
--- src/test/java/com/mateacademy/lessons16/ipa/OrderDaoImplTest.java
+++ src/test/java/com/mateacademy/lessons16/ipa/OrderDaoImplTest.java
@@ -1,9 +1,10 @@
 package com.mateacademy.lessons16.ipa;
 
 import com.mateacademy.lessons16.entry.Orders;
-import com.mateacademy.lessons16.ipa.*;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
@@ -11,6 +12,29 @@ import java.util.Date;
 
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OrderDaoImplTest {
- 
+    private static final BigDecimal NOT_EXIST_ORDER = BigDecimal.valueOf(-1);
+    private static final BigDecimal ALREADY_EXIST_ORDER = BigDecimal.valueOf(112922);
+    private static final Orders ORDER = new Orders(BigDecimal.valueOf(12352), null, null, null, new Date(), null,
+            BigDecimal.valueOf(10), null);
+    private OrderDao orderDao = new OrderDaoImpl();
+
+    @Test
+    public void insertOrder() throws SQLException {
+        assertTrue(orderDao.insertOrder(ORDER)) ;
+    }
+
+    @Test
+    public void test2updateOrder() throws SQLException {
+        ORDER.setAmount(BigDecimal.valueOf(10));
+        ORDER.setQty(BigDecimal.valueOf(10));
+        assertTrue(orderDao.updateOrder(ORDER));
+    }
+
+    @Test
+    public void test3deleteOrder() throws SQLException {
+        assertTrue(orderDao.deleteOrder(ORDER.getOrderNum()));
+    }
+
 }
