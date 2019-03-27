53b96b74baa7788d478db63a5e5f302331d839bb
Sergey Klunniy <s.klunniy@gmail.com>
2019-03-27 14:25:13 +0200

    I changed immutable class

--- src/main/java/mate/academy/hw02/ImmutableInt.java
+++ src/main/java/mate/academy/hw02/ImmutableInt.java
@@ -2,12 +2,18 @@ package mate.academy.hw02;
 
 public class ImmutableInt {
     private final int value;
+    private int size;
 
-    public ImmutableInt(int i) {
+    public ImmutableInt(int i, int size) {
         value = i;
+        this.size = size;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
