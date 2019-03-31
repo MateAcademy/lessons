8f7e003cb3cd31913dd446712f683242cf99f284
Sergey Klunniy <s.klunniy@gmail.com>
2019-03-31 17:28:03 +0300

    commit 31.03.2019 hw

---
+++ src/main/java/mate/academy/hw4/myArrayList/SimpleArray.java
@@ -0,0 +1,58 @@
+package mate.academy.hw4.myArrayList;
+
+import java.util.Iterator;
+
+public class SimpleArray<E> implements Simple<E> {
+    private E[] values;
+
+    public SimpleArray() {
+        values = (E[]) new Object[0];
+    }
+
+    @Override
+    public boolean add(E e) {
+        try {
+            E[] temp = values;
+            values = (E[]) new Object[temp.length + 1];
+            System.arraycopy(temp, 0, values, 0, temp.length);
+            values[values.length - 1] = e;
+            return true;
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public void delete(int index) {
+        try {
+            E[] temp = values;
+            values = (E[]) new Object[temp.length - 1];
+            System.arraycopy(temp, 0, values, 0, index);
+            int amountEmemAfterIndex = temp.length - index - 1;
+            System.arraycopy(temp, index + 1, values, index, amountEmemAfterIndex);
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public E get(int index) {
+        return values[index];
+    }
+
+    @Override
+    public int size() {
+        return values.length;
+    }
+
+    @Override
+    public void update(int index, E e) {
+        values[index] = e;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new ArrayIterator<E>(values);
+    }
+}
